69e23fefb87b56f7b5db32e11b23163e13e512e7
Lorena Lobo <74051141+loaalobo@users.noreply.github.com>
2022-02-05 09:17:28 -0300

    feat: adiciona lógica para validação do preenchimento dos campos do formulário

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 // Add condicional para só chamar a função se os inputs estiverem preenchidos
 
-
-function calcularConsumo() {
-
-    const volumeLitrosTanque = document.getElementById("volumeLitrosTanque").value;
+function validaForm() {
+    const volumeLitrosTanque = document.getElementById("volumeLitrosTanque").value;  
     const kmTanqueGasolina = document.getElementById("kmTanqueGasolina").value;
     const kmTanqueAlcool = document.getElementById("kmTanqueAlcool").value;
     const resultado = document.getElementById("resultado");
 
-    const consumoGasolina = kmTanqueGasolina / volumeLitrosTanque;
-    const consumoAlcool = kmTanqueAlcool /volumeLitrosTanque;
+    if(volumeLitrosTanque == "" || kmTanqueGasolina == "" || kmTanqueAlcool == "") {
+        alert('Por favor, preencha todos os campos antes de prosseguir.');
+    } else {
+        exibeResultado();
+    }
+}
 
 
-    resultado.innerHTML = "Resultado: <br>Gasolina: " + consumoGasolina + " km/litro <br>Álcool: " + consumoAlcool + " km/litro";
+function retornaVolumeLitrosTanque() {
+    return volumeLitrosTanque = document.getElementById("volumeLitrosTanque").value;
+}
+
+function calculaConsumoGasolina() { 
+    const kmTanqueGasolina = document.getElementById("kmTanqueGasolina").value;
+    return consumoGasolina = (kmTanqueGasolina / retornaVolumeLitrosTanque()).toFixed(2);    
+}
+
+function calculaConsumoAlcool() {
+    const kmTanqueAlcool = document.getElementById("kmTanqueAlcool").value;
+    return consumoAlcool = (kmTanqueAlcool / retornaVolumeLitrosTanque()).toFixed(2);
+}
+
+function exibeResultado() {
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "Resultado: <br>Gasolina: " + calculaConsumoGasolina() + " km/litro <br>Álcool: " + calculaConsumoAlcool()+ " km/litro";
 }
